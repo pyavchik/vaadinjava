e146c07f9998a753d4a3ea7ac338cc554c61cdb8
eugeneshapovalov <shapovalovei@gmail.com>
2018-08-20 15:35:55 +0300

    upd

--- src/test/java/VaadinTaTest.java
+++ src/test/java/VaadinTaTest.java
@@ -1,6 +1,11 @@
+import com.google.common.base.Function;
 import io.trueautomation.client.driver.TrueAutomationDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,9 +25,18 @@ public class VaadinTaTest {
     private By mainScreenNotes = By.xpath(ta("vaadinDashboard:MainScreen:notes", "//textarea[@rows='5']"));
     private By mainScreenUserMenu = By.xpath(ta("vaadinDashboard:MainScreen:userMenu", "(//span[@class='v-menubar-menuitem-caption'])[1]"));
     private By mainScreenEditProfile = By.xpath(ta("vaadinDashboard:MainScreen:editProfile", "//span[text()='Edit Profile']"));
+
+
     private By profileScreenTitle = By.id(ta("vaadinDashboard:ProfileScreen:title", "gwt-uid-16"));
     private By profileScreenSexMale = By.xpath(ta("vaadinDashboard:ProfileScreen:sexMale", "//div[@id='gwt-uid-18']//span[contains(., 'Male')]"));
     private By profileScreenSexFemale = By.xpath(ta("vaadinDashboard:ProfileScreen:sexFemale", "//div[@id='gwt-uid-18']//span[contains(., 'Female')]"));
+
+    private By profileScreenFirstName = By.id(ta("vaadinDashboard:ProfileScreen:firstName", "gwt-uid-12"));
+    private By profileScreenSecondName = By.id(ta("vaadinDashboard:ProfileScreen:secondName", "gwt-uid-14"));
+
+    private By profileScreenSave = By.xpath(ta("vaadinDashboard:ProfileScreen:save", "//div[contains(@class, 'v-button-primary')]"));
+
+
 
     @BeforeTest
     public void before(){
@@ -33,26 +47,52 @@ public class VaadinTaTest {
     @Test
     public void vaadinTest() throws InterruptedException {
         driver.get(baseUrl);
-        Thread.sleep(5000);
+
+        fluentWait(closePopUpBtn);
         driver.findElement(closePopUpBtn).click();
-        Thread.sleep(5000);
+
+        fluentWait(emailFl);
         driver.findElement(emailFl).sendKeys("True Automation");
         driver.findElement(passwordFl).sendKeys("admin");
         driver.findElement(signInBtn).click();
 
-        Thread.sleep(5000);
-        driver.findElement(mainScreenNotes).sendKeys("\nTrue Automation\nBest Test Automation Framework");
-        Thread.sleep(5000);
-        driver.findElement(mainScreenUserMenu).click();
-        driver.findElement(mainScreenEditProfile).click();
-        Thread.sleep(5000);
-        driver.findElement(profileScreenTitle).sendKeys("Mrs.");
-        driver.findElement(profileScreenTitle).click();
-        Thread.sleep(5000);
-        driver.findElement(profileScreenSexMale).click();
-        driver.findElement(profileScreenSexFemale).click();
-        Thread.sleep(5000);
+        for(int i = 0; i < 2; i++) {
+
+            fluentWait(mainScreenNotes);
+            driver.findElement(mainScreenNotes).sendKeys("\nTrue Automation\nBest Test Automation Framework");
+            driver.findElement(mainScreenUserMenu).click();
+
+            fluentWait(mainScreenEditProfile);
+            driver.findElement(mainScreenEditProfile).click();
+
+            fluentWait(profileScreenFirstName);
+            driver.findElement(profileScreenFirstName).sendKeys("Douglas");
+            driver.findElement(profileScreenSecondName).sendKeys("Engelbart");
+
+            driver.findElement(profileScreenTitle).sendKeys("Mrs.");
+            driver.findElement(profileScreenTitle).click();
+
+            driver.findElement(profileScreenSexMale).click();
+            driver.findElement(profileScreenSexFemale).click();
+
+            driver.findElement(profileScreenSave).click();
+        }
     }
+
+    public WebElement fluentWait(final By locator) {
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(5, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        });
+
+        return  foo;
+    };
 
     @AfterTest
     public void after(){
--- src/test/java/VaadinTest.java
+++ src/test/java/VaadinTest.java
@@ -1,12 +1,19 @@
+import com.google.common.base.Function;
 import io.trueautomation.client.driver.TrueAutomationDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
+
+import static io.trueautomation.client.TrueAutomationHelper.ta;
 
 
 public class VaadinTest {
@@ -24,6 +31,13 @@ public class VaadinTest {
     private By profileScreenSexMale = By.xpath("//div[@id='gwt-uid-18']//span[contains(., 'Male')]");
     private By profileScreenSexFemale = By.xpath("//div[@id='gwt-uid-18']//span[contains(., 'Female')]");
 
+    private By profileScreenFirstName = By.id(ta("vaadinDashboard:ProfileScreen:firstName"));
+    private By profileScreenSecondName = By.id(ta("vaadinDashboard:ProfileScreen:secondName"));
+
+    private By profileScreenSave = By.xpath(ta("vaadinDashboard:ProfileScreen:save"));
+
+
+
     @BeforeTest
     public void before(){
         driver = new ChromeDriver();
@@ -33,26 +47,56 @@ public class VaadinTest {
     @Test
     public void vaadinTest() throws InterruptedException {
         driver.get(baseUrl);
-        Thread.sleep(5000);
+
+        fluentWait(closePopUpBtn);
         driver.findElement(closePopUpBtn).click();
-        Thread.sleep(5000);
+
+        fluentWait(emailFl);
         driver.findElement(emailFl).sendKeys("True Automation");
         driver.findElement(passwordFl).sendKeys("admin");
         driver.findElement(signInBtn).click();
 
-        Thread.sleep(5000);
-        driver.findElement(mainScreenNotes).sendKeys("\nTrue Automation\nBest Test Automation Framework");
-        Thread.sleep(5000);
-        driver.findElement(mainScreenUserMenu).click();
-        driver.findElement(mainScreenEditProfile).click();
-        Thread.sleep(5000);
-        driver.findElement(profileScreenTitle).sendKeys("Mrs.");
-        driver.findElement(profileScreenTitle).click();
-        Thread.sleep(5000);
-        driver.findElement(profileScreenSexMale).click();
-        driver.findElement(profileScreenSexFemale).click();
-        Thread.sleep(5000);
+        for(int i = 0; i < 2; i++) {
+
+            fluentWait(mainScreenNotes);
+            driver.findElement(mainScreenNotes).sendKeys("\nTrue Automation\nBest Test Automation Framework");
+
+            Thread.sleep(2000);
+            driver.findElement(mainScreenUserMenu).click();
+
+
+
+            fluentWait(mainScreenEditProfile);
+            driver.findElement(mainScreenEditProfile).click();
+
+            fluentWait(profileScreenFirstName);
+            driver.findElement(profileScreenFirstName).sendKeys("Douglas");
+            driver.findElement(profileScreenSecondName).sendKeys("Engelbart");
+
+            driver.findElement(profileScreenTitle).sendKeys("Mrs.");
+            driver.findElement(profileScreenTitle).click();
+
+            driver.findElement(profileScreenSexMale).click();
+            driver.findElement(profileScreenSexFemale).click();
+
+            driver.findElement(profileScreenSave).click();
+        }
     }
+
+    public WebElement fluentWait(final By locator) {
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(5, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        });
+
+        return  foo;
+    };
 
     @AfterTest
     public void after(){
